f83ccedf27f4d0c9299825fe64b9c43fc8e4cd1e
Rathnayake M.R.T.N <99705847+IT21253294tharindu@users.noreply.github.com>
2024-05-05 19:12:09 +0530

    Update home.jsx

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -31,7 +31,7 @@ export default function Home() {
                 <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl animate-fadeInUp">
                   {/* Add animation class */}
                   Embark on an interstellar journey with our innovative web
-                  application.
+                  Application.
                   <br />
                 </h2>
                 <p className="mt-6 text-lg leading-8 text-gray-300 animate-fadeInUp delay-2000">
