98f19a8879602c591a4a395e01743c5739020f6f
Rathnayake M.R.T.N <99705847+IT21253294tharindu@users.noreply.github.com>
2024-05-05 10:37:28 +0530

    Update landingpage.jsx

--- frontend/src/pages/landingpage.jsx
+++ frontend/src/pages/landingpage.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { Suspense, lazy, useEffect } from "react";
 import ParticlesBackground from "../components/particlesbackground";
 import Home from "../components/home";
 import Header from "../components/header";
 
+// Lazy load the ParticlesBackground component
+const LazyParticlesBackground = lazy(() =>
+  import("../components/particlesbackground")
+);
+
 // Landing page component
 export default function LandingPage() {
+  useEffect(() => {
+    import("../components/particlesbackground");
+  }, []);
   return (
     <>
-      <ParticlesBackground />
+      <Suspense fallback={<div>Loading...</div>}>
+        <LazyParticlesBackground />
+      </Suspense>
       <Header />
       <Home />
     </>
